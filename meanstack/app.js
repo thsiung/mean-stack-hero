efa1597299369b50edd21d7c7fdc7745b4c0e60f
thsiung <thsiung@users.noreply.github.com>
2016-03-08 13:03:25 -0500

    more debugging

--- meanstack/app.js
+++ meanstack/app.js
@@ -8,20 +8,6 @@ helloworld.push("obj2");
 console.log(helloworld);
 
 console.log("hahahaha");
-<<<<<<< HEAD
-console.log("hahahaha");
-console.log("hahahaha");
-console.log("hahahaha");
-console.log("hahahaha");
-console.log("hahahaha");
-console.log("hahahaha");
-console.log("hahahaha");
-console.log("hahahaha");
-console.log("hahahaha");
-console.log("hahahaha");
-console.log("hahahaha");
-=======
-console.log("testing1");
-console.log("testing2");
-console.log("testing3");
->>>>>>> 75a20d18d0914e261f035efd181cb80b7e965b46
+
+console.log("debugging");
+console.log("debugging");
